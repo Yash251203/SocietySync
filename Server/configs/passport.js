7a2a0336dec47a399c31a18d5ea348ddad21223a
Rhythmdoshi31 <rhythmdoshi04@gmail.com>
2025-05-02 16:13:35 +0530

    Intermediate login process

---
+++ Server/configs/passport.js
@@ -0,0 +1,22 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const user = require('../models/userModel');
+const dotenv = require('dotenv');
+dotenv.config();
+
+passport.use(new GoogleStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: process.env.GOOGLE_CALLBACK_URL,
+}, async (accessToken, refreshToken, profile, done) => {
+  const existingUser = await user.findOne({ googleId: profile.id });
+  if (existingUser) return done(null, existingUser);
+
+  const newUser = new user({
+    googleId: profile.id,
+    name: profile.displayName,
+    email: profile.emails[0].value
+  });
+  await newUser.save();
+  done(null, newUser);
+}));

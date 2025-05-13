9ae8d6573ca5cf3829be8527bfd9b620cade5e6d
Rhythmdoshi31 <rhythmdoshi04@gmail.com>
2025-05-13 13:06:24 +0530

    Fix: cors

--- Server/app.js
+++ Server/app.js
@@ -10,7 +10,7 @@ connectDB();
 
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://society-sync-neon.vercel.app',
   credentials: true
 }));
 // Serve static files from the 'public' directory
